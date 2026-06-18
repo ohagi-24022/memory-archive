1c557595c70147b017d84383ca0124cc1a182651
ohagi-24022 <ep24022-3165@sti.chubu.ac.jp>
2026-06-18 23:39:59 +0900

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: process.env.VITE_BASE_PATH || "/",
   plugins: [react()],
+  base: '/memory-archive/',
   build: {
     emptyOutDir: false,
   },
